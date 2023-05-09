a0f8ac5a0214118ede7221b7174bc4d3411b3822
sonjahalikka <122718291+sonjahalikka@users.noreply.github.com>
2023-05-09 10:12:59 +0300

    Sonjan ja Marian työ

---
+++ lesson01.py
@@ -0,0 +1,32 @@
+#*****************************************************  
+
+# Tehtava???.py kaytetaan …. Kirjoita tahan koodin  
+
+#  toimintakuvaus. Minecraft harjoitus 01, harjoitellaan forkkausta Githubiin.
+
+# @author tekija: (Maria Rantanen & Sonja Hälikkä, Sh & MR)  
+
+# @since pvm: (versio pvm 9.5.2023)  
+
+# @version versio: (versionumero 1.0)  
+
+# muutos: (nimikirjaimet omat nimikirjaimet)  
+
+#*****************************************************/ 
+
+# 01 lesson 1/3
+
+player.say("Hi")
+
+#01 lesson 2/3
+player.say("To show")
+player.say("text on")
+player.say("the screen")
+player.say("we use quotes")
+player.say("2")
+player.say(2)
+
+#01 lesson 3/3
+player.say(45+2505)
+player.say(255/15)
+player.say((27+4)*(55-52))
